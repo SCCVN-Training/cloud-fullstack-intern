46383adcac684354a52e8d38c30b495e7fe72d2c
nguyenhaohongduscc <du.nguyenhaohong@scc.com>
2026-07-17 12:05:54 +0100

    fix: spec test of login and register component
    Fix the spec test of login and register component missing the provideRouter

--- frontend-angular/src/app/features/auth/pages/login/login.component.spec.ts
+++ frontend-angular/src/app/features/auth/pages/login/login.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -9,6 +10,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LoginComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
--- frontend-angular/src/app/features/auth/pages/register/register.component.spec.ts
+++ frontend-angular/src/app/features/auth/pages/register/register.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
@@ -9,6 +10,7 @@ describe('RegisterComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RegisterComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
