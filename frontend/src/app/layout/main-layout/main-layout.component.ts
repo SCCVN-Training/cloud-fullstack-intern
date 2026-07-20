8084da11e6650e9b30e672dae5c31c8e2f71ce9e
SCC-NDTMinh <Minh.NguyenDoTuan@scc.com>
2026-07-20 09:52:26 +0100

    fix: resolve errors when displaying the sidebar

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { NavbarComponent } from '../navbar/navbar.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
 import { FooterComponent } from '../footer/footer.component';
