24e828e0418756c57b174d59bf2a254ffddd91ab
nguyenhaohongduscc <du.nguyenhaohong@scc.com>
2026-07-21 05:56:32 +0100

    fix(frontend): fix spec of not found component

--- frontend-angular/src/app/shared/components/not-found/not-found.component.spec.ts
+++ frontend-angular/src/app/shared/components/not-found/not-found.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { provideRouter } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 
 describe('Loading', () => {
@@ -9,6 +10,7 @@ describe('Loading', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [NotFoundComponent],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(NotFoundComponent);
