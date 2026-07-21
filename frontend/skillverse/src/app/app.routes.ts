9a605203d97d56d2c0a1b0959ffb6dd173d38a5f
NhuSCC <nhu.lenguyenquynh@scc.com>
2026-07-21 07:59:26 +0100

    Update app route

--- frontend/skillverse/src/app/app.routes.ts
+++ frontend/skillverse/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 
 import { PublicLayoutComponent } from './shared/components/layout/public-layout/public-layout.component';
+
 import { HomepageComponent } from './features/homepage/homepage.component';
+
 import { HowItWorksPage } from './features/how-it-works/how-it-works';
 import { AboutUsPage } from './features/about-us/about-us';
+import { BrowseSkillsPage } from './features/skill-pages/browse-skills/browse-skills';
 
 export const routes: Routes = [
   {
@@ -21,6 +24,10 @@ export const routes: Routes = [
       {
         path: 'about-us',
         component: AboutUsPage
+      },
+      {
+        path: 'browse-skills',
+        component: BrowseSkillsPage
       }
     ]
   },
