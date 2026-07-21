e67d5d1961ff8b8a75783dbaf074b79675afa79d
NhuSCC <nhu.lenguyenquynh@scc.com>
2026-07-21 11:26:44 +0100

    Update app

--- frontend/skillverse/src/app/app.ts
+++ frontend/skillverse/src/app/app.ts
@@ -1,10 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToastComponent } from './shared/components/toast/toast.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: '<router-outlet></router-outlet>',
+  imports: [
+    RouterOutlet,
+    ToastComponent
+  ],
+  template: `
+    <router-outlet></router-outlet>
+    <app-toast></app-toast>
+  `,
   styleUrl: './app.scss'
 })
 export class App {
