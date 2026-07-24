0e7aabddad9d26879ed0b130d3cb611da6cf99ac
NhuSCC <nhu.lenguyenquynh@scc.com>
2026-07-24 07:36:15 +0100

    feat (header): Update auth & nav

--- frontend/skillverse/src/app/shared/components/header/header.component.ts
+++ frontend/skillverse/src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth/auth';
 
 interface NavigationItem {
@@ -11,37 +11,37 @@ interface NavigationItem {
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink,
-    RouterLinkActive
-  ],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  constructor(public authService: AuthService) {}
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+  ) {}
 
   logout() {
-  this.authService.logout();
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 
   readonly navigationItems: NavigationItem[] = [
     {
       label: 'Home',
-      route: '/'
+      route: '/',
     },
     {
       label: 'Browse Skills',
-      route: '/browse-skills'
+      route: '/browse-skills',
     },
     {
       label: 'About Us',
-      route: '/about-us'
+      route: '/about-us',
     },
     {
       label: 'How It Works',
-      route: '/how-it-works'
-    }
+      route: '/how-it-works',
+    },
   ];
 }
