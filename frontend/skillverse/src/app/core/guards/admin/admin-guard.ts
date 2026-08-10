a892379f4dbe276f3836613cf74332d4cc82579e
NhuSCC <nhu.lenguyenquynh@scc.com>
2026-08-10 08:15:46 +0100

    chore (auth): Update role and implement admin guard

---
+++ frontend/skillverse/src/app/core/guards/admin/admin-guard.ts
@@ -0,0 +1,21 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth/auth';
+
+export const adminGuard: CanActivateFn = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const user = authService.currentUser();
+
+  if (!user) {
+    return router.createUrlTree(['/login']);
+  }
+
+  if (user.role !== 'admin') {
+    return router.createUrlTree(['/homepage']);
+  }
+
+  return true;
+};
