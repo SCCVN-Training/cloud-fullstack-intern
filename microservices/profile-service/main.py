33192c15539ed2c4bfc66d3046aa8632e5fdc76f
nguyenhaohongduscc <du.nguyenhaohong@scc.com>
2026-09-03 03:59:57 +0100

    fix(backend): enable trace logging in span

--- microservices/profile-service/main.py
+++ microservices/profile-service/main.py
@@ -50,28 +50,28 @@ app = FastAPI(
     redoc_url="/redoc",
 )
 
-# if not settings.is_development:
-#     try:
-#         from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-
-#         # Attach X-Ray tracking to all FastAPI endpoints automatically
-#         FastAPIInstrumentor.instrument_app(app)
-#         logger.info(
-#             "✅ OpenTelemetry instrumentation is ENABLED for cloud environment."
-#         )
-#     except ImportError:
-#         logger.error(
-#             "❌ OpenTelemetry packages are not installed. Tracing is DISABLED."
-#         )
-# else:
-#     logger.info("⚠️ Running in DEVELOPMENT mode. OpenTelemetry tracing is DISABLED.")
-
 if not settings.is_development:
-    # Do we are already using opentelemetry-instrument command wrapper,
-    # we just log the status here to avoid double-instrumentation conflicts.
-    logger.info("✅ OpenTelemetry instrumentation is ENABLED via command wrapper.")
+    try:
+        from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+
+        # Attach X-Ray tracking to all FastAPI endpoints automatically
+        FastAPIInstrumentor.instrument_app(app)
+        logger.info(
+            "✅ OpenTelemetry instrumentation is ENABLED for cloud environment."
+        )
+    except ImportError:
+        logger.error(
+            "❌ OpenTelemetry packages are not installed. Tracing is DISABLED."
+        )
 else:
     logger.info("⚠️ Running in DEVELOPMENT mode. OpenTelemetry tracing is DISABLED.")
+
+# if not settings.is_development:
+#     # Do we are already using opentelemetry-instrument command wrapper,
+#     # we just log the status here to avoid double-instrumentation conflicts.
+#     logger.info("✅ OpenTelemetry instrumentation is ENABLED via command wrapper.")
+# else:
+#     logger.info("⚠️ Running in DEVELOPMENT mode. OpenTelemetry tracing is DISABLED.")
 
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
