c58d912904419029aa4a6a72dafaec76ed485dad
DuyNguyenSCC <Duy.Nguyen@scc.com>
2026-09-09 09:56:33 +0100

    added urlencoding

--- terraform-v2/data/secrets.tf
+++ terraform-v2/data/secrets.tf
@@ -22,7 +22,7 @@ resource "aws_secretsmanager_secret_version" "app_initial" {
   secret_id = aws_secretsmanager_secret.app.id
 
   secret_string = jsonencode({
-    DATABASE_URL         = "postgresql://${var.db_username}:${jsondecode(data.aws_secretsmanager_secret_version.rds_secret_version.secret_string)["password"]}@${aws_db_instance.postgres.endpoint}/${var.project_name}?sslmode=require"
+    DATABASE_URL         = "postgresql://${var.db_username}:${urlencode(jsondecode(data.aws_secretsmanager_secret_version.rds_secret_version.secret_string)["password"])}@${aws_db_instance.postgres.endpoint}/${var.project_name}?sslmode=require"
     SECRET_KEY           = "initial-secret-key-change-in-aws-secrets-manager"
     ALGORITHM            = "HS256"
     ACCESS_TOKEN_EXPIRE  = "30"
