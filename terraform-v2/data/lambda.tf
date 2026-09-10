e5000050d2657bec9cc1ae6970bfab8b98c545fd
DuyNguyenSCC <Duy.Nguyen@scc.com>
2026-09-10 13:10:42 +0100

    added script to init the rds

--- terraform-v2/data/lambda.tf
+++ terraform-v2/data/lambda.tf
@@ -49,7 +49,7 @@ resource "aws_lambda_function" "trash_purge" {
       ENVIRONMENT          = var.environment
       BUCKET_NAME          = aws_s3_bucket.storage.bucket
       BUCKET_REGION_NAME   = var.aws_region
-      DATABASE_URL         = "postgresql://${var.db_username}@${aws_db_instance.postgres.endpoint}/${var.project_name}"
+      DATABASE_URL         = "postgresql://${var.db_username}:${urlencode(jsondecode(data.aws_secretsmanager_secret_version.rds_secret_version.secret_string)["password"])}@${aws_db_instance.postgres.endpoint}/${var.project_name}?sslmode=require"
       API_STR              = "/api/v2"
       STORAGE_QUOTA_BYTES  = "21474836480"
       SECRET_KEY           = "lambda-internal-placeholder-051004"
